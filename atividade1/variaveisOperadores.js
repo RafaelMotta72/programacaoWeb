b0c5848a22ee3ab6e6ce0b4543eb9ac49f249a4f
RAFAEL MOTTA MACHADO <rafael.motta@a.ucb.br>
2026-03-09 21:20:11 -0300

    Update variaveisOperadores.js

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,12 @@
+let numero1 = 10;
+let numero2 = 5;
+
+let resultadoSoma = numero1 + numero2;
+let resultadoSubtracao = numero1 - numero2;
+let resultadoMultiplicacao = numero1 * numero2;
+let resultadoDivisao = numero1 / numero2;
+
+console.log(`Resultado da soma entre ${numero1} e ${numero2} é ${resultadoSoma}`);
+console.log(`Resultado da subtração entre ${numero1} e ${numero2} é ${resultadoSubtracao}`);
+console.log(`Resultado da multiplicação entre ${numero1} e ${numero2} é ${resultadoMultiplicacao}`);
+console.log(`Resultado da divisão entre ${numero1} e ${numero2} é ${resultadoDivisao}`);
