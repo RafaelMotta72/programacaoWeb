0f5813e75ff760c66bee77cee1098eb76fa9b68a
RAFAEL MOTTA MACHADO <rafael.motta@a.ucb.br>
2026-04-27 20:41:27 -0300

    Aula dia 27/04

--- app_agendamento_consulta/index.js
+++ app_agendamento_consulta/index.js
@@ -3,10 +3,22 @@ const mustacheExpress = require('mustache-express');
 const PORT = 8080;
 const app = express();
 
+app.engine('html', mustacheExpress());
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+app.use(express.urlencoded({extended: true}));
+
 app.listen(PORT, ()=>{
     console.log('app rodando na porta ' + PORT);
 });
 
+app.post('/agendamento', (req, res)=>{
+    dados_agendamento = req.body;
+    console.log(dados_agendamento)
+    res.render('agendamento.html', {dados_agendamento});
+});
+
+
 app.get("/", (req,res)=>{
-    res.send("Agendamento de consulta");
+    res.render("index.html");
 })
